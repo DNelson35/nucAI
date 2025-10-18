519a092ad2dd39604e15d359eca7306f524c5548
Damien Nelson <dhnisaman@gmail.com>
2025-10-18 00:35:44 -0400

    Set up Prompt and response

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from nucai!")
+import os
+import sys
+import time
+from dotenv import load_dotenv
+from google import genai 
 
+if len(sys.argv) < 2:
+  print("pwease provide a pwompt uwu")
+  time.sleep(2)
+  sys.exit(1)
+ 
+prompt = sys.argv[1]
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model = 'gemini-2.0-flash-001',
+    contents = prompt
+)
+
+print(response.text,"\n","Prompt tokens:",response.usage_metadata.prompt_token_count,"\n","Response tokens:",response.usage_metadata.candidates_token_count)
+
